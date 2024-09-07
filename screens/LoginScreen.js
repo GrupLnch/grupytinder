ba457629c97f3474b4d45a8ed94ec673b161692c
calvin1011 <calvinssendawula@gmail.com>
2024-09-06 23:50:51 -0500

    able to change value in useAuth fule to test the authentication. User logged in if we have a name placeholder, user signed out and on login screen if user is null

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
-        const { user } = useAuth()
-        console.log(user);
-
     return (
         <View>
             <Text>Login to the app</Text>
