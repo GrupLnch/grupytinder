bd1df6abfc77248a507b3b17bdfa252d2dd68bf6
calvin1011 <calvinssendawula@gmail.com>
2024-09-11 03:39:22 -0500

    Login screen added for google but has 404 error that Authorization error

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,20 +1,41 @@
-import React, {createContext, useContext} from "react";
-import * as Google from "expo-auth-session";
+import React, {createContext, useContext, useEffect} from "react";
+import {useIdTokenAuthRequest} from "expo-auth-session/providers/google";
 
-const AuthContext = createContext({
-    // Initial state context
-})
+const AuthContext = createContext({});
+
+const config = {
+    androidClientId: '316379143309-au5c79ocs75s1e48tcpvqghit0erp42h.apps.googleusercontent.com',
+    iosClientId: '316379143309-060fa2q928k8af5c8jh57d95da4s3e26.apps.googleusercontent.com',
+    scopes: ["profile", "email"],
+    permissions: ["public_profile", "email", "gender", "location"],
+}
 
 export const AuthProvider = ({ children }) => {
+    const [request, response, promptAsync] = useIdTokenAuthRequest({
+        clientId: config.androidClientId,
+    });
+
+    useEffect(() => {
+        if (response?.type === 'success') {
+            const { id_token } = response.params;
+            // Handle successful login (e.g., save id_token or authenticate with Firebase)
+            console.log("Google login success: ", id_token);
+        }
+    }, [response]);
 
     const signInWithGoogle = async () => {
-        await Google.logInAsync()
-    }
+        try {
+            await promptAsync();
+        } catch (error) {
+            console.error("Sign-in error: ", error);
+        }
+    };
 
 
     return (
         <AuthContext.Provider value={{
-            user: "null",
+            user: null,
+            signInWithGoogle
         }}>
             { children }
         </AuthContext.Provider>
